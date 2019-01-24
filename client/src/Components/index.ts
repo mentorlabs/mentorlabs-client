c032976ea8588cdd747a598cecef8d5e1a290b1d
Ely Alamillo <ely0013@gmail.com>
2019-01-24 00:33:19 -0600

    wired up new home page

--- client/src/Components/index.ts
+++ client/src/Components/index.ts
@@ -1,3 +1,4 @@
 export * from "./App";
 export * from "./Navigation";
 export * from "./Footer";
+export * from "./Home";
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Navigation, Footer } from "./Components";
+import { Navigation, Footer, Home } from "./Components";
 
 const Index = () => <h2>Home</h2>;
 const About = () => <h2>About</h2>;
@@ -11,7 +11,7 @@ export const AppRouter = () => (
     <React.Fragment>
       <Navigation />
       <div>
-        <Route path="/" exact={true} component={Index} />
+        <Route path="/" exact={true} component={Home} />
         <Route path="/about/" component={About} />
         <Route path="/users/" component={Users} />
       </div>
