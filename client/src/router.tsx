0199acfd20881fb8597d8d08790109b31fd31704
Ely Alamillo <ely0013@gmail.com>
2019-02-02 22:29:55 -0600

    wiredup new signup container

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Navigation, Footer, Home, Signup } from "./Components";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Navigation, Footer, Home, EnhancedSignup } from './Components';
 
 export const AppRouter = () => (
   <Router>
@@ -8,7 +8,7 @@ export const AppRouter = () => (
       <Navigation />
       <div>
         <Route path="/" exact={true} component={Home} />
-        <Route path="/signup/" exact={true} component={Signup} />
+        <Route path="/signup/" exact={true} component={EnhancedSignup} />
       </div>
       <Footer />
     </React.Fragment>
