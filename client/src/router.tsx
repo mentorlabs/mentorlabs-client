9fe3860244e94a6bb00ca8fdc2bc7bed6d11b7f3
Ely Alamillo <ely0013@gmail.com>
2019-02-03 16:10:12 -0600

    wired up new signup component

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Navigation, Footer, Home, EnhancedSignup } from './Components';
+import { Navigation, Footer, Home, Signup } from './Components';
 
 export const AppRouter = () => (
   <Router>
@@ -8,7 +8,7 @@ export const AppRouter = () => (
       <Navigation />
       <div>
         <Route path="/" exact={true} component={Home} />
-        <Route path="/signup/" exact={true} component={EnhancedSignup} />
+        <Route path="/signup/" exact={true} component={Signup} />
       </div>
       <Footer />
     </React.Fragment>
