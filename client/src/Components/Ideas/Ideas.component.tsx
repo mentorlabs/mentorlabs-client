39c4a35cb2e46da4b2345421714e3f49a980fff4
Ely Alamillo <ely0013@gmail.com>
2019-03-02 16:05:54 -0600

    add link to newIdea form

--- client/src/Components/Ideas/Ideas.component.tsx
+++ client/src/Components/Ideas/Ideas.component.tsx
@@ -1,34 +1,39 @@
 import React from 'react';
+
 import { IdeasCard } from './IdeasCard/IdeasCard.component';
+import { Link, Route } from 'react-router-dom';
+import { WrapWithRouterProps } from '../../types';
 
 import './Ideas.scss';
-import { Link } from 'react-router-dom';
-
-export const Ideas = () => (
-  <React.Fragment>
-    <section className="hero is-info is-bold is-medium">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title is-size-1">Project Ideas</h1>
-          <h2 className="subtitle">Some compelling description.</h2>
-          <div>
-            <Link to="/idea">
-              <button className="button is-link">Submit Idea</button>
-            </Link>
+export const Ideas: React.SFC<WrapWithRouterProps<{}>> = ({ match }) => {
+  // tslint:disable:no-console
+  console.log(match);
+  return (
+    <React.Fragment>
+      <section className="hero is-info is-bold is-medium">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title is-size-1">Project Ideas</h1>
+            <h2 className="subtitle">Some compelling description.</h2>
+            <div>
+              <Link to={`${match.url}/new`}>
+                <button className="button is-link">Submit Idea</button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <div className="container">
-      <div className="columns is-multiline ideas-wrapper">
-        <IdeasCard />
-        <IdeasCard />
-        <IdeasCard />
-        <IdeasCard />
-        <IdeasCard />
-        <IdeasCard />
+      <div className="container">
+        <div className="columns is-multiline ideas-wrapper">
+          <IdeasCard />
+          <IdeasCard />
+          <IdeasCard />
+          <IdeasCard />
+          <IdeasCard />
+          <IdeasCard />
+        </div>
       </div>
-    </div>
-  </React.Fragment>
-);
+    </React.Fragment>
+  );
+};
